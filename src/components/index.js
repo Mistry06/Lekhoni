c5378d5b798e9aebf72e59bb0f8068fa8bb64f7b
Somnath Kar <somnathkar612005@gmail.com>
2025-05-27 21:21:18 +0530

    Fix: Applied consistent PascalCase to component filenames and updated all relevant import paths, including fixing footer component typos. Resolves Vercel build errors.

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ import Select from "./select";
 import Header from "./header/header";
 import Footer from "./footer/footer";
 import Container from "./container/container";
-import Logo from "./Logo";
+import Logo from "./Logo.jsx";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import Signup from "./Signup";
